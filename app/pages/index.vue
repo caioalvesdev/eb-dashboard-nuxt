fe656a751665a045cb6570965f8d28da398cb7d6
Caio Alves Ximenes da Silva <kaio_rx@hotmail.com>
2025-11-03 15:17:55 -0300

    feat: refactor logout functionality to use dedicated handleLogout method

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,23 +12,29 @@ const { data: profiles } = await useAsyncData(
   },
   { default: () => [] }
 );
+async function handleLogout() {
+  await client.auth.signOut();
+  navigateTo("/auth/signin");
+}
 </script>
 <template>
-  <div class="flex flex-col gap-4">
+  <UContainer>
     <div class="flex flex-col gap-4">
-      <template v-for="profile in profiles" :key="profile.id">
-        <!-- <UAvatar :src="profile.avatar_url" /> -->
-        <UUser
-          :name="profile.username"
-          :description="profile.email"
-          :avatar="{ src: profile.avatar_url, icon: 'i-lucide-image' }"
-        />
-      </template>
+      <div class="flex flex-col gap-4">
+        <template v-for="profile in profiles" :key="profile.id">
+          <!-- <UAvatar :src="profile.avatar_url" /> -->
+          <UUser
+            :name="profile.username"
+            :description="profile.email"
+            :avatar="{ src: profile.avatar_url, icon: 'i-lucide-image' }"
+          />
+        </template>
+      </div>
+      <div>
+        <UButton label="sair" @click="handleLogout" />
+      </div>
     </div>
-    <div>
-      <UButton label="sair" @click="client.auth.signOut()" />
-    </div>
-  </div>
+  </UContainer>
 </template>
 
 <style lang="scss" scoped></style>
